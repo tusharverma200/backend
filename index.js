eec11575bb1ae979d5b2ac191db9fb27fa6fe8fc
Tushar Verma <vermatushar200@gmail.com>
2024-01-20 16:52:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/login', (req, res)=>{
+    res.send('<h1>Welcome to login page</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
